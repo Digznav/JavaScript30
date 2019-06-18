c0176c5d4403f6600017d800b13aa88b9e83ee55
Priscila Martinez <priscila.mtz.fraire@gmail.com>
2019-06-18 10:14:57 -0500

    Exercise 19 📹📸

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,48 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+var width = 320;
+var height = 0;
+
+function takePhoto (e) {
+    e.preventDefault();
+    takePicture();
+}
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+.then(function(stream) {
+    video.srcObject = stream;
+    video.play();
+})
+.catch(function(err) {
+    console.log('An error occurred: ' + err);
+});
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="plop">`;
+
+    strip.insertBefore(link, strip.firstChild);
+}
+
+video.addEventListener('canplay', paintToCanvas);
