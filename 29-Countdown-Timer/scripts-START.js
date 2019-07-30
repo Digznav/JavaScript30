a8e6bd9fd0b13ca3523d87c49e10550851893aea
Gabriela Rodriguez <aeka.snape@gmail.com>
2019-07-30 10:03:36 -0500

    Ex 29

---
+++ 29-Countdown-Timer/scripts-START.js
@@ -0,0 +1,4 @@
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
